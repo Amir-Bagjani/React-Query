fe59fdb23d5dcf6d80d8141f9e587aab1b4fac29
amirbagjni <amirbagjani@gmail.com>
2022-02-20 04:21:27 +0330

    error

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -2,15 +2,16 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const fetchData = () => {
-  return axios.get("http://localhost:3030/superheroes");
+  return axios.get("http://localhost:3030/superheroes1");
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-hero", fetchData);
+  const { isLoading, data, isError, error } = useQuery("super-hero", fetchData);
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
       {isLoading && <h2>Loading...</h2>}
+      {isError && <h2>{error.message}</h2>}
       {data?.data.map(item => (
         <p key={item.id}>{item.name}</p>
       ))}
