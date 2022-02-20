3d56df00e714379fab6e8ea8a48300f94b47aefe
amirbagjni <amirbagjani@gmail.com>
2022-02-20 16:47:57 +0330

    on click useQuery

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -4,19 +4,21 @@ import { useQuery } from "react-query";
 const fetchData = () => axios.get("http://localhost:3030/superheroes");
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-hero",
     fetchData,
     { 
-      refetchInterval: 3000,
-      refetchIntervalInBackground: true
+      enabled: false
     }
   );
+
   console.log({ isFetching, isLoading });
+
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
-      {isLoading && <h2>Loading...</h2>}
+      <button onClick={refetch}>Fetch Data</button>
+      {(isLoading || isFetching) && <h2>Loading...</h2>}
       {isError && <h2>{error.message}</h2>}
       {data?.data.map((item) => (
         <p key={item.id}>{item.name}</p>
