ff345d9fd5f92b0a70c4bae77527c3043f58d1df
amirbagjni <amirbagjani@gmail.com>
2022-02-20 17:47:43 +0330

    transform data

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,26 +1,15 @@
 import axios from "axios";
-import { useState } from "react";
 import { useQuery } from "react-query";
 
 const fetchData = () => axios.get("http://localhost:3030/superheroes");
 
 export const RQSuperHeroesPage = () => {
-  const [interval,setInterval] = useState(2000)
-  const onSuccess = (data) =>{
-    console.log(`Success`, data) 
-    if(data.data.length >= 4) setInterval(false)
-  }
-  const onError = (error) => {
-    console.log(`Success`, error);
-    setInterval(false)
-  }
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+  
+  const { isLoading, data, isError, error, isFetching } = useQuery(
     "super-hero",
     fetchData,
     { 
-      refetchInterval: interval,
-      onSuccess,
-      onError
+      select: (data) => data.data.map(i => i.name)
     }
   );
 
@@ -29,11 +18,13 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
-      {/* <button onClick={refetch}>Fetch Data</button> */}
       {(isLoading || isFetching) && <h2>Loading...</h2>}
       {isError && <h2>{error.message}</h2>}
-      {data?.data.map((item) => (
+      {/* {data?.data.map((item) => (
         <p key={item.id}>{item.name}</p>
+      ))} */}
+      {data?.map(hero => (
+        <div key={hero}>{hero}</div>
       ))}
     </>
   );
