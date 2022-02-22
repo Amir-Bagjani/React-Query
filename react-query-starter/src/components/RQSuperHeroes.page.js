7d22a4ef45321c4bfb2d30262e1e7c91f7050b97
amirbagjni <amirbagjani@gmail.com>
2022-02-23 00:45:45 +0330

    delete and edit

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import useQueryHook, { useAddData } from "../hooks/useQueryHook";
+import useQueryHook, { useAddData, useEditData, useRemoveData } from "../hooks/useQueryHook";
 
 
 export const RQSuperHeroesPage = () => {
@@ -11,12 +11,20 @@ export const RQSuperHeroesPage = () => {
   
   const { isLoading, data, isError, error } = useQueryHook()
   const { mutate: add } = useAddData()
+  const { mutate: edit } = useEditData()
+  const { mutate: remove } = useRemoveData()
 
 
   const handleAdd = () => {
     add({name, alterEgo})
     setAlterEgo("")
     setName("")
+  }
+  const handleEdit = (hero) => {
+    edit({...hero, name: hero.name + 'edited'})
+  }
+  const handleRemove = (id) => {
+    remove(id)
   }
 
 
@@ -37,6 +45,8 @@ export const RQSuperHeroesPage = () => {
       {data?.data.map((item) => (
         <p key={item.id}>
           <Link to={`rq-super-heroes/${item.id}`}>{item.name}</Link>
+          <button onClick={() => handleEdit(item)}>Edit Hero</button>
+          <button onClick={() => handleRemove(item.id)}>Remove Hero</button>
         </p>
       ))}
 
--- react-query-starter/src/hooks/useQueryHook.js
+++ react-query-starter/src/hooks/useQueryHook.js
@@ -4,6 +4,8 @@ import { request } from "../utils/axios-utils";
 
 const fetchData = () => request({url: '/superheroes'});
 const addData = (hero) => request({url: '/superheroes', method: 'post', data: hero});
+const editData = (hero) => request({url: `/superheroes/${hero.id}`, method: 'put', data: hero});
+const removeData = (id) => request({url: `/superheroes/${id}`, method: 'delete'});
 
 const useQueryHook = () => {
   return useQuery("super-heroes", fetchData);
@@ -17,6 +19,35 @@ export const  useAddData = () => {
       const previousData = queryClient.getQueryData("super-heroes")
       queryClient.setQueryData("super-heroes", (oldQueryData) => {
         return {...oldQueryData, data: [...oldQueryData.data, {id: Math.random(), newHero}]}
+      })
+      return {previousData}
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData("super-heroes", context.previousData)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries("super-heroes")
+    },
+  })
+}
+
+export const  useEditData = () => {
+  const queryClient = useQueryClient()
+  return useMutation(editData, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("super-heroes")
+    }
+  })
+}
+
+export const  useRemoveData = () => {
+  const queryClient = useQueryClient()
+  return useMutation(removeData, {
+    onMutate: async(newHero) => {
+      await queryClient.cancelQueries("super-heroes");
+      const previousData = queryClient.getQueryData("super-heroes")
+      queryClient.setQueryData("super-heroes", (oldQueryData) => {
+        return {...oldQueryData, data: oldQueryData.data.filter( i => i.id !== Number(newHero.id))}
       })
       return {previousData}
     },
